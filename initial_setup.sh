50cb2a5101a3c14d05c4d76d5cdd3ae5f3bb8143
Mabe <bokaj-eu@web.de>
2026-03-27 15:37:04 +0100

    docker installation

--- initial_setup.sh
+++ initial_setup.sh
@@ -5,7 +5,23 @@ apt update
 apt upgrade -y
 
 # Docker installation
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Add Docker's official GPG key:
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 docker run hello-world
 
